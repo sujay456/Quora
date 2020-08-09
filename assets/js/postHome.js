92dfddbdb7e965679d978ccaca336d7d5078dbcd
sujay456 <sujaystark811@gmail.com>
2020-08-09 16:45:26 +0530

    now can add link to the answers

--- assets/js/postHome.js
+++ assets/js/postHome.js
@@ -10,6 +10,7 @@ function showOptions(){
 
 
     // this is to toggleclass on the clicked menu
+    // console.log('this ran');
     $(' .options-for-question',self).toggleClass('display');
 
     event.stopPropagation();
@@ -36,6 +37,23 @@ function show2nav()
     
     
     let parents=$(event.target).parentsUntil('div.controls');
+    console.log(parents);
+
+    let link=$(' input',$(parents[0]))[0].value;
+    console.log(link);
+
+    // For appending the link to the text area
+
+    let parentsInput=$(event.target).parentsUntil('div.not-answered');
+
+    let textarea=$(' .input div',$(parentsInput[parentsInput.length-1]));
+    // console.log(textarea)
+    htmlLink=$(`<a href=http://${link}>${link}</a>`);
+
+    // console.log(htmlLink[0]);
+
+    textarea[0].append(htmlLink[0]);
+    // console.log(textarea[0].innerText);
 
     $(parents[parents.length-1]).css('top','-43px');
 }
@@ -44,19 +62,63 @@ function show2nav()
 
 function blue()
 {
-    $(event.target).toggleClass('active');
-}
+        let parentFar=$(event.target).parentsUntil('.not-answered');
+        console.log(parentFar);
+        let input= $(' .input div',parentFar[parentFar.length-1]);
+    if($(event.target).children().length==0)
+    {
+        $(event.target).toggleClass('active');
+        let parents=$(event.target).parentsUntil('.primary_controls');
+        let inputType=$(' input',parents[0]);
+       
+        if(inputType[0].value=='bold')
+        {
+            
+            input.toggleClass('bold');
+            
+        }
+        else
+        {
+            input.toggleClass('italic');
+        }
+        
+    }   
+    else
+    {
+        $(' i',$(event.target)).toggleClass('active');
+        let inputType=$(' input',$(event.target));
 
+        
+
+        if(inputType[0].value=='bold')
+        {
+            
+            input.toggleClass('bold');
+            
+        }
+        else
+        {
+            input.toggleClass('italic');
+        }
+
+    }
+    // let parents=$(event.target).parentsUntil('.primary_controls');
+    
+    
+
+    // console.log(event.target);
+
+}
 
 
 
 function showAnswerTab()
 {
-    console.log(event);
+    // console.log(event);
 
     let parents=$(event.target).parentsUntil('div#questions-for-you');
 
-    console.log(parents);
+    // console.log(parents);
 
     $(' .textarea-for-answering',$(parents[parents.length-1])).toggleClass('display');
     
