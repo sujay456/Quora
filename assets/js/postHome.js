3a5c43a50fbcc78d143ac185ff981a2ef2a45b7d
sujay456 <sujaystark811@gmail.com>
2020-08-09 12:13:57 +0530

    complted frontend of the former ,fixed some bugs

--- assets/js/postHome.js
+++ assets/js/postHome.js
@@ -19,4 +19,45 @@ function showOptions(){
 $(document).click(function(){
     // console.log('clicked');
     $('.options-for-question').addClass('display');
-})
+});
+
+
+// for transisitioning between  1 and 2
+function show1nav(){
+
+  
+    let parents=$(event.target).parentsUntil('div.controls');
+    $(parents[parents.length-1]).css('top','0px');
+
+}
+
+function show2nav()
+{
+    
+    
+    let parents=$(event.target).parentsUntil('div.controls');
+
+    $(parents[parents.length-1]).css('top','-43px');
+}
+
+// for making the bold and italic work
+
+function blue()
+{
+    $(event.target).toggleClass('active');
+}
+
+
+
+
+function showAnswerTab()
+{
+    console.log(event);
+
+    let parents=$(event.target).parentsUntil('div#questions-for-you');
+
+    console.log(parents);
+
+    $(' .textarea-for-answering',$(parents[parents.length-1])).toggleClass('display');
+    
+}
